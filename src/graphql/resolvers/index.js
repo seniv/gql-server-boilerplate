f3735c1e0a327a7e8943a87fe157443ed3ec83fc
Ivan Seniv <seniv97@gmail.com>
2019-02-16 20:38:52 +0200

    used package "merge-graphql-schemas" to merge schemas and resolvers

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,12 +1,10 @@
-import { reduce, mergeDeepRight } from 'ramda';
+import { mergeResolvers } from 'merge-graphql-schemas';
 
 import { Scalars } from './scalars';
 import { User } from './user';
 import { Auth } from './auth';
 import { Question } from './question';
 import { Answer } from './answer';
-
-const mergeResolvers = reduce(mergeDeepRight, []);
 
 export const resolvers = mergeResolvers([
   Scalars,
--- src/graphql/schemas/index.js
+++ src/graphql/schemas/index.js
@@ -1,17 +1,18 @@
 import { flatten } from 'ramda';
+import { mergeTypes } from 'merge-graphql-schemas';
 
-import Root from './root.gql';
 import { Scalars } from './scalars';
 import { User } from './user';
 import { Auth } from './auth';
 import { Question } from './question';
 import { Answer } from './answer';
 
-export const typeDefs = flatten([
-  Root,
+export const typeDefs = mergeTypes(flatten([
   Scalars,
   User,
   Auth,
   Question,
   Answer,
-]);
+]));
+
+console.log('merged', typeDefs);

77681f14a28d62cf38717c70490291f66627be61
Ivan Seniv <seniv97@gmail.com>
2019-04-06 15:41:43 +0300

    make field "email" on User type private & add query to load user by ID

--- src/graphql/resolvers/user/query.js
+++ src/graphql/resolvers/user/query.js
@@ -1,3 +1,6 @@
+import { User } from '../../../models/user';
+
 export const Query = {
   currentUser: (_, __, { user }) => user,
+  user: (_, { id }) => User.findById(id),
 };
--- src/graphql/resolvers/user/types.js
+++ src/graphql/resolvers/user/types.js
@@ -1,7 +1,13 @@
+import R from 'ramda';
+
 import { Question } from '../../../models/question';
 import { Answer } from '../../../models/answer';
 
 export const userTypes = {
   questions: ({ _id }) => Question.find({ createdById: _id }),
   answers: ({ _id }) => Answer.find({ createdById: _id }),
+  // Field "Email" is private, and will be shown only for owner of the email
+  email: ({ _id, email }, _, context) => (
+    R.pathEq(['user', '_id'], _id, context) ? email : null
+  ),
 };
